bdd49f6f26479d65a10acfc4dd75faaae124baf9
Long-Soeng168 <generalpurpose167@gmail.com>
2025-02-11 20:19:38 +0700

    update

--- src/app/dashboard/categories/components/my-loading-animation.tsx
+++ src/app/dashboard/categories/components/my-loading-animation.tsx
@@ -1,14 +1,3 @@
-// import React from "react";
-
-// const MyLoadingAnimation = () => {
-//   return (
-//     <div className="flex justify-center">
-//       <div className="w-16 h-16 mb-8 border-t-4 border-solid rounded-full border-primary animate-spin"></div>
-//     </div>
-//   );
-// };
-
-// export default MyLoadingAnimation;
 "use client";
 import Lottie from "lottie-react";
 import animationData from "@/animations/loading-animation2.json";
